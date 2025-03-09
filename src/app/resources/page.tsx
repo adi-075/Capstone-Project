ee244cf4fcd600f3fd7cb7200164ed767531eeee
Aditya <37655818+adi-075@users.noreply.github.com>
2025-03-09 17:04:44 -0400

    Initial Commit

---
+++ src/app/resources/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { TopBar } from '@/components/Dashboard/TopBar'
+
+export default function Resources() {
+    return (
+        <div className='bg-white rounded-lg shadow h-screen p-2'>
+            <TopBar />
+        </div>
+    )
+}
