c2664247b6b21366af596f908ef8a0f909a7c30a
Aditya <37655818+adi-075@users.noreply.github.com>
2025-03-11 18:12:16 -0400

    Dashboard Grid

--- src/components/Dashboard/Grid.tsx
+++ src/components/Dashboard/Grid.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { StatCards } from './StatCards';
 import { ActivityGraph } from './ActivityGraph';
-import { UsageRadar } from './UsageRadar';
+import UsageRadar from './UsageRadar';
+import StudentDashboardChart from './ActivityChart';
 
 export const Grid = () => {
     return (
         <div className='px-4 grid gap-3 grid-cols-12'>
             <StatCards />
-            <ActivityGraph />
+            {/* <ActivityGraph /> */}
+            <StudentDashboardChart />
             <UsageRadar />
         </div>
     )
