02be29eaf0aff5ad7c60a9aef0361bd05c06c33e
Aditya <37655818+adi-075@users.noreply.github.com>
2025-03-11 01:51:23 -0400

    Update Course page

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { TopBar } from '@/components/Dashboard/TopBar'
+import Course from './course'
 
 export default function Courses() {
     return (
-        <div className='bg-white rounded-lg shadow h-screen p-2'>
+        <div className='bg-white rounded-lg shadow p-2'>
             <TopBar />
+            <Course />
         </div>
     )
 }
