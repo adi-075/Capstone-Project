8381597abdeb89cf115cf3ac6d017016f03b407e
Aditya <37655818+adi-075@users.noreply.github.com>
2025-03-25 13:28:32 -0400

    Add supabase client

---
+++ src/app/utils/supabaseClient.tsx
@@ -0,0 +1,12 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY
+console.log('Supabase Key:', supabaseKey ? 'Key exists' : 'Key is undefined')
+
+if (!supabaseKey) {
+    throw new Error('SUPABASE_KEY is not defined in environment variables')
+}
+
+const supabase = createClient(supabaseUrl, supabaseKey)
+export default supabase
