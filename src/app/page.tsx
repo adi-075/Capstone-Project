10a599f137c489ff8c7adfaac6708ea7c3bf985a
Aditya <37655818+adi-075@users.noreply.github.com>
2025-03-25 12:21:56 -0400

    Update homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Dashboard } from "@/components/Dashboard/Dashboard";
 
 export default function Home() {
   return (
-    <main className="">
+    <main>
       <Dashboard />
     </main>
   );
