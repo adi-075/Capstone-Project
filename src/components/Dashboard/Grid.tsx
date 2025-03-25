1178a3564fea6200c10c66e75d983d4dfaa90745
Aditya <37655818+adi-075@users.noreply.github.com>
2025-03-25 12:22:40 -0400

    Include GradingGrid in dashboard's grid layout

--- src/components/Dashboard/Grid.tsx
+++ src/components/Dashboard/Grid.tsx
@@ -1,14 +1,21 @@
 import React from 'react'
 import { StatCards } from './StatCards';
+import { GradingGrid } from '../Courses Grading/GradingGrid';
 import UsageRadar from './UsageRadar';
 import StudentDashboardChart from './ActivityChart';
 
 export const Grid = () => {
     return (
-        <div className='px-4 grid gap-3 grid-cols-12'>
+        <><div className='px-4 pb-5 grid gap-3 grid-cols-12'>
+            <h1 className='col-span-12 text-2xl font-bold' > Metrics</h1 >
             <StatCards />
             <StudentDashboardChart />
             <UsageRadar />
-        </div>
+            <h1 className='col-span-12 text-2xl font-bold mt-1'>Courses</h1>
+        </div >
+            <div className='px-4 pb-5 grid gap-10 grid-cols-12'>
+                <GradingGrid />
+            </div>
+        </>
     )
 };
