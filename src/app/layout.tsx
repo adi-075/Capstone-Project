f5a3b89b21b65840bd3e56849a7b61f49a2ac92e
Aditya <37655818+adi-075@users.noreply.github.com>
2025-03-25 00:03:34 -0400

    Add metadata to layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,11 @@
 "use client";
+import { Inter } from "next/font/google";
 import { Sidebar } from "@/components/Sidebar/Sidebar";
 import "@/app/globals.css";
 import { usePathname } from "next/navigation";
-import { inter } from "./fonts/fonts";
+import { metadata } from "@/app/metadata";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function DashboardLayout({
   children,
@@ -13,6 +16,12 @@ export default function DashboardLayout({
 
   return (
     <html lang="en">
+      <head>
+        <title>{String(metadata.title)}</title>
+        <meta name="description" content={metadata.description || ""} />
+        <meta name="keywords" content={Array.isArray(metadata.keywords) ? metadata.keywords.join(", ") : ""} />
+        <meta name="author" content={Array.isArray(metadata.authors) ? metadata.authors.map(author => author.name ?? "").join(", ") : ""} />
+      </head>
       <body className={`${inter.className} text-stone-950 bg-stone-100`}>
         {pathname !== "/login" ? (
           <main className="md:grid gap-4 p-4 grid-cols-[220px_1fr]">
---
+++ src/app/metadata.ts
@@ -0,0 +1,8 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "RocketDash",
+  description: "RocketDash is a platform for students to manage their courses and resources.",
+  keywords: ["app", "next.js", "example"],
+  authors: [{ name: "Team A-5" }],
+}; 
