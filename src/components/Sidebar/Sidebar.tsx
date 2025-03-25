ab0e7c1082d3ecea310241e4db161471096f0225
Aditya <37655818+adi-075@users.noreply.github.com>
2025-03-24 23:55:53 -0400

    Conditionally render sidebar when it's not login route

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,10 +1,19 @@
+"use client";
 import React from "react";
+import { usePathname } from "next/navigation";
 import { RocketDash } from "./AccountToggle";
 import { Search } from "./Search";
 import { RouteSelect } from "./RouteSelect";
 // import { Plan } from "./Plan";
 
 export const Sidebar = () => {
+  const pathname = usePathname();
+
+  // Do not render the sidebar on the login route
+  if (pathname === "/login") {
+    return null;
+  }
+
   return (
     <div>
       <div className="sticky top-4 h-[calc(100vh-32px-48px)]">
