28f52e8f2456e0d558907f5d16c097c1ecd37137
Aditya <37655818+adi-075@users.noreply.github.com>
2025-03-27 12:26:08 -0400

    Make AccountToggle use dynamic data

--- src/components/Sidebar/AccountToggle.tsx
+++ src/components/Sidebar/AccountToggle.tsx
@@ -1,8 +1,21 @@
-import React from 'react'
-import { FiChevronDown, FiChevronUp } from 'react-icons/fi'
-import Image from 'next/image'
+'use client'
 
-export const AccountToggle = () => {
+import { useEffect, useState } from 'react'
+import { fetchStudents } from '@/app/api/supabase'
+import Image from 'next/image'
+import { FiChevronDown, FiChevronUp } from 'react-icons/fi'
+
+export function AccountToggle() {
+    const [student, setStudent] = useState<any>(null)
+
+    useEffect(() => {
+        const getStudent = async () => {
+            const students = await fetchStudents()
+            setStudent(students?.[0])
+        }
+        getStudent()
+    }, [])
+
     return (
         <div className='border-b mb-4 mt-2 pb-4 border-stone-300'>
             <button className='flex p-0.5 hover:bg-stone-200 rounded transition-colors relative gap-2 w-full items-center'>
@@ -16,7 +29,9 @@ export const AccountToggle = () => {
                 </div>
                 <div className='text-start'>
                     <span className="text-sm font-bold block">Aditya</span>
-                    <span className='text-xs block text-stone-500 break-words w-28'>amhambr@rockets.utoledo.edu</span>
+                    <span className='text-xs block text-stone-500 break-words w-28'>
+                        {student?.email || 'Loading...'}
+                    </span>
                 </div>
                 <FiChevronDown className='absolute right-2 top-1/2 translate-y-[calc(-50%+4px)] text-xs' />
                 <FiChevronUp className='absolute right-2 top-1/2 translate-y-[calc(-50%-4px)] text-xs' />
