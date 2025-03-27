b82d75a7be6fe12739772c74094ae2d8f0eff7a2
Aditya <37655818+adi-075@users.noreply.github.com>
2025-03-27 12:25:35 -0400

    Refactor UsageRadar to PerformanceRadar

--- src/components/Dashboard/Grid.tsx
+++ src/components/Dashboard/Grid.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { StatCards } from './StatCards';
 import { GradingGrid } from '../Courses Grading/GradingGrid';
-import UsageRadar from './UsageRadar';
+import PerformanceRadar from './PerformanceRadar';
 import StudentDashboardChart from './ActivityChart';
 
 export const Grid = () => {
@@ -10,7 +10,7 @@ export const Grid = () => {
             <h1 className='col-span-12 text-2xl font-bold' > Metrics</h1 >
             <StatCards />
             <StudentDashboardChart />
-            <UsageRadar />
+            <PerformanceRadar />
             <h1 className='col-span-12 text-2xl font-bold mt-1'>Courses</h1>
         </div >
             <div className='px-4 pb-5 grid gap-10 grid-cols-12'>
