ff7a327c9fa71a87a4499f3bade793d723768f03
Aditya <37655818+adi-075@users.noreply.github.com>
2025-04-01 16:59:21 -0400

    Fix the events page height

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,14 +1,14 @@
-export const dynamic = 'force-dynamic'
+export const dynamic = "force-dynamic";
 
-import React from 'react'
-import { TopBar } from '@/components/Dashboard/TopBar'
-import EventList from '@/app/events/EventList'
+import React from "react";
+import { TopBar } from "@/components/Dashboard/TopBar";
+import EventList from "@/app/events/EventList";
 
 export default function Events() {
-    return (
-        <div className='bg-white rounded-lg shadow h-screen p-2'>
-            <TopBar />
-            <EventList />
-        </div>
-    )
+  return (
+    <div className="bg-white rounded-lg shadow p-2">
+      <TopBar />
+      <EventList />
+    </div>
+  );
 }
