7cd33c2b772302d3287ce8f35abf8abd98e2a571
Aditya <37655818+adi-075@users.noreply.github.com>
2025-04-01 16:56:31 -0400

    Update supabaseClient to also contain your baseUrl

--- src/app/utils/supabaseClient.tsx
+++ src/app/utils/supabaseClient.tsx
@@ -1,5 +1,13 @@
 import { createClient } from "@supabase/supabase-js";
+import { headers } from 'next/headers';
 
+export async function defineBaseUrl() {
+    const headersList = headers();
+    const protocol = process.env.NODE_ENV === 'development' ? 'http' : 'https';
+    const host = (await headersList).get('host') || 'localhost:3000';
+    const baseUrl = `${protocol}://${host}`;
+    return baseUrl;
+}
 // This client is for server-side use only
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
 const supabaseKey =
