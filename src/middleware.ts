0e6879bf35dedbd5df763fe39dbad4b7f72bf3e7
Aditya <37655818+adi-075@users.noreply.github.com>
2025-04-01 20:27:07 -0400

    Update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,41 +1,12 @@
-import { NextRequest, NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server';
+import { updateSession } from '@/app/utils/supabase/middleware';
 
-export function middleware(request: NextRequest) {
-    const pathname = request.nextUrl.pathname
-    const isProd = process.env.NODE_ENV === 'production'
-
-    if (!isProd || !pathname.startsWith('/api/')) {
-        return NextResponse.next()
-    }
-
-    const referer = request.headers.get('referer') || ''
-    const origin = request.headers.get('origin') || ''
-    const host = request.headers.get('host') || ''
-
-    const isInternal =
-        referer.includes(host) ||
-        origin.includes(host) ||
-        (!referer && !origin) // SSR requests from Node don't send either
-
-    if (!isInternal) {
-        return new NextResponse(
-            JSON.stringify({
-                message: "🚫 External access to this API route is blocked in production.",
-                hint: "This route is only accessible by Rocketdash frontend or server.",
-                timestamp: new Date().toISOString(),
-            }),
-            {
-                status: 403,
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-            }
-        )
-    }
-
-    return NextResponse.next()
+export async function middleware(req: NextRequest) {
+  const res = NextResponse.next();
+  await updateSession(req, res);
+  return res;
 }
 
 export const config = {
-    matcher: ['/api/:path*'],
-}
+  matcher: ['/dashboard/:path*'],
+};
