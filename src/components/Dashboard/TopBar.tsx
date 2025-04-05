d3c9198fd997e9cfe0ef74e94a288764a52740aa
adi-075 <adityamhambrey323@gmail.com>
2025-04-04 22:19:33 -0500

    Add loading state to topbar

--- src/components/Dashboard/TopBar.tsx
+++ src/components/Dashboard/TopBar.tsx
@@ -1,28 +1,10 @@
 import React from "react";
 import { getFormattedDate } from "@/app/utils/dateFetch";
 import { getStudents } from "@/lib/getStudents";
+import { TopBarClient } from "./TopBarClient";
 
 export const TopBar = async () => {
     const students = await getStudents();
 
-    return (
-        <div className="border-b px-4 mb-4 mt-2 pb-4 border-stone-200">
-            <div className="flex items-center justify-between p-0.5">
-                <div>
-                    {students && students[0] ? (
-                        <span className="text-2xl font-bold block">
-                            🚀 Hi, {students[0].first_name}!
-                        </span>
-                    ) : (
-                        <span className="text-2xl font-bold block">
-                            🚀 Welcome!
-                        </span>
-                    )}
-                    <span className="text-lg block text-stone-500">
-                        {getFormattedDate()}
-                    </span>
-                </div>
-            </div>
-        </div>
-    );
+    return <TopBarClient students={students} />;
 };
