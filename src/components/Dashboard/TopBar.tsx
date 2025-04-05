ed770aaa927d838b626d8048602f26a08cb08098
adi-075 <adityamhambrey323@gmail.com>
2025-04-04 23:48:49 -0500

    Remove loading ui from topbar

--- src/components/Dashboard/TopBar.tsx
+++ src/components/Dashboard/TopBar.tsx
@@ -1,9 +1,28 @@
 import React from "react";
 import { getStudents } from "@/lib/getStudents";
-import { TopBarClient } from "./TopBarClient";
+import { getFormattedDate } from "@/app/utils/dateFetch";
 
 export const TopBar = async () => {
     const students = await getStudents();
 
-    return <TopBarClient students={students} />;
+    return (
+        <div className="border-b px-4 mb-4 mt-2 pb-4 border-stone-200">
+            <div className="flex items-center justify-between p-0.5">
+                <div>
+                    {students && students[0] ? (
+                        <span className="text-2xl font-bold block">
+                            🚀 Hi, {students[0].first_name}!
+                        </span>
+                    ) : (
+                        <span className="text-2xl font-bold block">
+                            🚀 Welcome!
+                        </span>
+                    )}
+                    <span className="text-lg block text-stone-500">
+                        {getFormattedDate()}
+                    </span>
+                </div>
+            </div>
+        </div>
+    );
 };
