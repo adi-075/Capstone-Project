ca2ebe95b3189ba494cd471c6334fbaf4e444201
adi-075 <adityamhambrey323@gmail.com>
2025-04-25 07:59:10 -0400

    Updating Favicon

--- src/app/metadata.ts
+++ src/app/metadata.ts
@@ -5,4 +5,14 @@ export const metadata: Metadata = {
   description: "RocketDash is a platform for students to manage their courses and resources.",
   keywords: ["app", "next.js", "example"],
   authors: [{ name: "Team A-5" }],
+  icons: {
+    icon: [
+      { url: '/favicon.ico', sizes: 'any' },
+      { url: '/favicon.png', sizes: '32x32' },
+      { url: '/favicon.png', sizes: '192x192' }
+    ],
+    apple: [
+      { url: '/favicon.png', sizes: '180x180' }
+    ]
+  }
 }; 
